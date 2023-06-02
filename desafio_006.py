a7a979786232a615dc7ea41c226e86291ee12f73
atnzpe <gleysonasilva@gmail.com>
2023-06-01 22:19:06 -0300

    Cria desafio 006

---
+++ desafio_006.py
@@ -0,0 +1,8 @@
+# Cria um algoritmo que leia um numero e mostre o seu dobro, seu triplo e a raiz quadrada
+
+n = int(input('Digite um número qualquer: '))
+dobro = n*2
+triplo = n*3
+r_quadrada = int(n**(1/2))
+
+print(f"O número digitado foi {n}, seu dobro é {dobro}, seu triplo é {triplo} e sua raiz quadrada é {r_quadrada}. ")
