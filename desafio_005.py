bdd8167f6d8fc6d6a3250c8b774ee5cecbd4498a
atnzpe <gleysonasilva@gmail.com>
2023-06-01 22:01:44 -0300

    Cria desafio 005

---
+++ desafio_005.py
@@ -0,0 +1,6 @@
+# Faça uma programa que leia um número Intyeiro e mostre na tela o seu sucessor e seu antecessor
+
+n = int(input('Digte um número qualquer: '))
+print(f'O número digitado é: {n}')
+print(f'O seu sucessor: {n+1}')
+print(f'O seu antecessor é: {n-1}')
