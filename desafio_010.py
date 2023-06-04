8214c0c2c725f101479f93976f7e5b2ff8215c0c
atnzpe <gleysonasilva@gmail.com>
2023-06-04 14:29:04 -0300

    Cria desafio 010

---
+++ desafio_010.py
@@ -0,0 +1,9 @@
+# Leia quanto uma pessoa tem na carteira e ela pode comprar em dolares
+# Cotação
+# US$ 1.00 = R$ 3.37
+
+v_em_real = float(input("Digite quantos reais você tem na carteira: "))
+real_convertido_dollar = v_em_real / 3.37
+
+print(f"Valor em dolar US$ {real_convertido_dollar}")
+
