a684a6efe2b01bb6a5a961260b26695824e6209f
atnzpe <gleysonasilva@gmail.com>
2023-06-05 17:24:38 -0300

    Cria código

--- desafio_011.py
+++ desafio_011.py
@@ -2,4 +2,12 @@
 # calcule a área e a quantidade de tinta para pintá-la.
 # Um litro pinta 2m²
 
+print("Um litro pinta 2m²\n")
+altura = float(input("Digite a altura: "))
+largura = float(input("Digite a largura: "))
+area = altura*largura
+qtd_tinta = area / 2
+
+print(f'A parede tem {round(area,2)} m², e você irá precisar de {round(qtd_tinta,2)} litro(s) de tinta.')
+
 
