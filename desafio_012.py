2712b5a0b513edc8dbb28db92e699dcf87229ba3
atnzpe <gleysonasilva@gmail.com>
2023-06-12 10:12:11 -0300

    Ajusta o código para desconto de 5%

--- desafio_012.py
+++ desafio_012.py
@@ -1,6 +1,6 @@
 #Leia o preço e mostre um novo preço com o acrescimo de 5%
 
 price = float(input("Digite o preço do item: "))
-new_price =  price+(price * 0.05)
+new_price =  price-(price * 0.05)
 
 print(f"Preço Antigo: R$ {price}\nPreço Atual: R$ {new_price}")
