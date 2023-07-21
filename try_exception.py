d4896e4b4a6d1adda21bdc3aee7b8f677663f255
atnzpe <gleysonasilva@gmail.com>
2023-07-20 21:59:09 -0300

    cria bloco try

---
+++ try_exception.py
@@ -0,0 +1,15 @@
+"""
+Introdução ao try/except
+
+try (tipo if) -> Tentar excutar o código
+except (tipo else)-> ocorreu algum erro ao tentar executar
+"""
+
+numero_str = input('Vou dobrar o numero que você digitar: ')
+
+try:
+    numero_float = float (numero_str)
+    print('FLOAT: ', numero_float)
+    print(f'O dobro de {numero_str} é {numero_str * 2:.0f}')
+except:
+    ...
