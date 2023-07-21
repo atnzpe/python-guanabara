8a8bcd84db6b820484035b83503e8f1f28edc74e
atnzpe <gleysonasilva@gmail.com>
2023-07-20 22:05:51 -0300

    teste ok

--- try_exception.py
+++ try_exception.py
@@ -8,9 +8,9 @@ except (tipo else)-> ocorreu algum erro ao tentar executar
 numero_str = input('Vou dobrar o numero que você digitar: ')
 
 try:
-    numero_float = float (numero_str)
-    print('FLOAT: ', numero_float)
+    numero_str = float (numero_str)
+    print('FLOAT: ', numero_str)
     print(f'O dobro de {numero_str} é {numero_str * 2:.0f}')
 except:
     print('Impresso no bloco except')
-    print('Isso não é um número')
+    print(f'Isso não é um número {numero_str}')
