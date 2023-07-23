d56fe1be036bef29f2f65b92d4f003b975dd011a
atnzpe <gleysonasilva@gmail.com>
2023-07-22 22:13:11 -0300

    cria variaveis e constantes

--- variavel_constant_codigo_complexo.py
+++ variavel_constant_codigo_complexo.py
@@ -5,3 +5,9 @@ Muitas condições no mesmo if (ruim)
 
 """
 
+velocidade = 61 # velocidade atual do carro
+local_carro = 90 # local do carro na estrada
+
+RADAR_1 = 60 # VELOCIDADE MÁXIMA RADAR 1
+LACAL_1 = 100 # local onde o radar 1 esta
+RADAR_RANGE = 1 #a distancia onde o radar pega
