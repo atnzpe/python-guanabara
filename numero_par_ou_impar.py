0d109a8b366eafcb9e1ed3afd63e5e4d4145ca05
atnzpe <gleysonasilva@gmail.com>
2023-07-26 22:26:52 -0300

    cria enunciado

---
+++ numero_par_ou_impar.py
@@ -0,0 +1,5 @@
+"""
+Faça um programa que peça ao usuario para digitar um numero inteiro,
+informe se este número é par ou impar. Caso o usuário não digite um número inteiro, informe que não é um número inteiro.
+
+"""
