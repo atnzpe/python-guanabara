4a7eeee63bd9693efb5ee30bbbd573f6db13a8a8
atnzpe <gleysonasilva@gmail.com>
2023-07-31 20:36:54 -0300

    cria código

--- bom_dia_boa_tarde_boa_noite.py
+++ bom_dia_boa_tarde_boa_noite.py
@@ -4,3 +4,19 @@ Faça um programa que pergunte a hora ao usuário e, baseando-se no horário des
  Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23
 """
 
+hour = input("Que horas são? ")
+
+convert_hour = int(hour)
+hour = convert_hour
+
+bom_dia = hour >= 0 and hour <= 11
+boa_tarde = hour >= 12 and hour <= 17
+boa_noite = hour >= 18 and hour <= 23
+
+
+if bom_dia:
+    print("bom dia!")
+elif boa_tarde:
+    print("Boa tarde!")
+elif boa_noite:
+    print("Boa Noite!")     
