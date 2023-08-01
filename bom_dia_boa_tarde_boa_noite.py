a6890021d1675263d58bd21efa4e82df2a81e30f
atnzpe <gleysonasilva@gmail.com>
2023-07-31 21:01:27 -0300

    inclui try and except

--- bom_dia_boa_tarde_boa_noite.py
+++ bom_dia_boa_tarde_boa_noite.py
@@ -3,20 +3,21 @@ Faça um programa que pergunte a hora ao usuário e, baseando-se no horário des
  exiba a saudação apropriada. Ex.
  Bom dia 0-11, Boa tarde 12-17 e Boa noite 18-23
 """
+try:
+    hour = input("Que horas são? ")
 
-hour = input("Que horas são? ")
+    hour_int = int(hour)
 
-convert_hour = int(hour)
-hour = convert_hour
+    bom_dia = hour >= 0 and hour <= 11
+    boa_tarde = hour >= 12 and hour <= 17
+    boa_noite = hour >= 18 and hour <= 23
 
-bom_dia = hour >= 0 and hour <= 11
-boa_tarde = hour >= 12 and hour <= 17
-boa_noite = hour >= 18 and hour <= 23
+    if bom_dia:
+        print("bom dia!")
+    elif boa_tarde:
+        print("Boa tarde!")
+    elif boa_noite:
+        print("Boa Noite!")
 
-
-if bom_dia:
-    print("bom dia!")
-elif boa_tarde:
-    print("Boa tarde!")
-elif boa_noite:
-    print("Boa Noite!")     
+except:
+    print("Voce não digitou um número!")
