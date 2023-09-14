9ca64423239188032219d40a9105191b25e5e654
Gleyson Atanázio <gleysonasilva@gmail.com>
2023-09-14 20:52:50 -0300

    atualiza enunciado do validar cpf

--- validar_cpf.py
+++ validar_cpf.py
@@ -4,7 +4,29 @@ CPF: 746.824.890-70
 
 Colete a soma dos 9 primeiros digitos so CPF
 multiplicando cada um dos valores por uma
-contagem regressiva começando de 10. 
+contagem regressiva começando de 10.
+
+Ex.: 746.824.890-70 (746824890)
+...10..9..8..7..6..5..4..3..2
+*..7...4..6..8..2..4..5..0..0
+...70..36.48.56.12.20.32.27.0
+
+Somar todos os resultados:
+70+36+48+56+12+20+32+27+0 = 301
+
+Multiplicar o resultado anterior por 10
+301 * 10 = 3010
+
+Obter o resto da divisão da conta anteriro por 11
+3010 % 11 = 7
+
+Se o resultado for maior que 9:
+    resultado é 0
+contrario disso:
+    resultado é o valor da conta
+
+
+
 
 """
 cpf = "74682489070"
@@ -16,7 +38,7 @@ contador = 0
 multiplicando cada um dos valores por uma
 contagem regressiva começando de 10. """
 
-#contador
+#contador regresssivo
 countdown = 10
 #Lista dos nove primeiros numeros
 nine_first_numbers = []
@@ -25,9 +47,11 @@ nine_first_numbers = []
 for i in cpf:
     number_multiplies_by_ten = int(i)*(countdown)
     nine_first_numbers.append(number_multiplies_by_ten)
+    print(i,countdown)
     countdown -=1
 #Obtem os nove primeiros numeros multiplicados pela contagem regressiva de 10 até zero
 nine_first_numbers = nine_first_numbers [:9]
+print(nine_first_numbers)
 
 #Cria uma variável que soma todos os itens da lista
 sum_nine_first_numbers = sum(nine_first_numbers)
@@ -38,7 +62,7 @@ multiplie_sum_nine_first_numbers = sum_nine_first_numbers * 10
 #Obter o resto da divisão de multiplie_sum_nine_first_numbers por 11
 resto_multiplie = multiplie_sum_nine_first_numbers % 11
 
-print('Resultado é 0' if resto_multiplie > 9 else f'resultado é {resto_multiplie}')
+print(f'Resultado é {resto_multiplie}' if resto_multiplie <= 9 else 0)
 
    
 
