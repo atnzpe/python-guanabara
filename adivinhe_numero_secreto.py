9e23c767a235949f7d700ba5d563a82f1f9bc118
Gleyson Atanázio <gleysonasilva@gmail.com>
2023-09-28 22:19:24 -0300

    Cria o advinhador de numero secreto

---
+++ adivinhe_numero_secreto.py
@@ -0,0 +1,24 @@
+secret_number = 777
+
+number = int(input("Digite aqui: "))
+
+while number != secret_number:
+    print(
+"""
++==============================+
+| Ha ha!                       |
+| Você está preso no meu loop! |
+|                              |      
++==============================+
+""")
+    number = int(input("Digite aqui: "))
+    
+print(
+"""
++========================+
+|                        |
+| Muito bem, trouxa!     |
+| Você está livre agora. |
+|                        |
++========================+
+""")
