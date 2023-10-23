b9ad422d58162b4848e83588805c5a52c138dcf9
Gleyson Atanázio <gleysonasilva@gmail.com>
2023-10-23 19:32:56 -0300

    funcao basica

---
+++ funcao_duplica_tripicla_quadruplica.py
@@ -0,0 +1,19 @@
+
+def duplica(numero):
+    return numero * 2
+
+def triplica(numero):
+    return numero * 3
+
+def quadruplica (numero):
+    return numero * 4
+    
+
+
+n = int(input("Digita um numnero: "))
+
+
+print(duplica(n))
+print(triplica(n))
+print(quadruplica(n))
+
