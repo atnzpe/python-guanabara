f85e156bfd314599cdfa13066d773cee65406b63
Gleyson Atanázio <gleysonasilva@gmail.com>
2023-10-24 20:34:55 -0300

    adicona o toal de vogais consoantes e espaços

--- come_vogais.py
+++ come_vogais.py
@@ -2,8 +2,12 @@ word = input('Digita um palavra: ')
 
 vowels = 'aeiou'
 
-lista_consoantes = [i for i in word if i not in vowels]
-lista_vogais = [i for i in word if i in vowels]
 
-print(f'Consoantes {lista_consoantes}')
-print(f'Vogais {lista_vogais}')
+lista_consoantes = [i for i in word if i not in vowels and i != " "]
+lista_vogais = [i for i in word if i in vowels]
+espacos = [i for i in word if i == " "]
+
+
+print(f'Total de Consoantes {len(lista_consoantes)} sua lista: {lista_consoantes}')
+print(f'Total de Vogais é {len(lista_vogais)} e sua lista: {lista_vogais}')
+print(f'Tota de espacos {len(espacos)}')
