05f0a512b864256ced32efef0400c42ae0d32776
Gleyson Atanázio <gleysonasilva@gmail.com>
2023-10-28 21:48:27 -0300

    Cria lista de perguntas

--- sistema_pergunta_E_resposta.py
+++ sistema_pergunta_E_resposta.py
@@ -2,6 +2,18 @@
 
 perguntas = [
     {
-        
+        'Pergunta': 'Quanto é 2+2?',
+        'Opções': ['1','2','3','4','5'],
+        'Resposta': '4',
+    },
+    {
+        'Pergunta': 'Quanto é 5+5?',
+        'Opções': ['25','55','10','51'],
+        'Resposta': '25',
+    },
+    {
+        'Pergunta': 'Quanto é 10/2?',
+        'Opções': ['4','5','2','3','20'],
+        'Resposta': '5'
     }
 ]
