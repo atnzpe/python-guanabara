d4af700b61e133849c8ae0326301b27eec9da5e3
Gleyson Atanázio <gleysonasilva@gmail.com>
2023-10-29 21:12:36 -0300

    Cria funçõa sorteia_pergunta

--- sistema_pergunta_E_resposta.py
+++ sistema_pergunta_E_resposta.py
@@ -1,5 +1,7 @@
 # Exercício - Sistema de Perguntas e Respostas
 
+import random
+
 perguntas = [
     {
         'Pergunta': 'Quanto é 2+2?',
@@ -9,11 +11,45 @@ perguntas = [
     {
         'Pergunta': 'Quanto é 5+5?',
         'Opções': ['25','55','10','51'],
-        'Resposta': '25',
+        'Resposta': '10',
     },
     {
         'Pergunta': 'Quanto é 10/2?',
         'Opções': ['4','5','2','3','20'],
-        'Resposta': '5'
+        'Resposta': '5',
     }
 ]
+
+
+def sorteia_pergunta(lista_perguntas):
+    pergunta_atual = random.choice(lista_perguntas)
+    print(pergunta_atual['Pergunta'])
+    print ('Escolha um Opção:')
+    for chave in pergunta_atual['Opções']:
+        print(chave)
+
+    while True:
+        resposta = input('Digite sua Escolha: ')
+
+        if resposta in pergunta_atual['Resposta']:
+            print(f'{resposta=}')
+            break
+        else:
+            print('Voce errou')
+            break
+
+    #return print(pergunta_atual['Pergunta'])
+
+
+
+sorteia_pergunta(perguntas)
+
+
+
+#for chave in pergunta_atual:
+    #print (pergunta_atual[chave])
+
+
+
+#print(perguntas[Pergunta])
+
