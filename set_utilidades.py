8511c7013ce57f6dec28d93c75d6dbf53363f1ba
Gleyson Atanázio <gleysonasilva@gmail.com>
2023-11-09 21:30:28 -0300

    Cria arquivo se com algumas de suas uilidades

---
+++ set_utilidades.py
@@ -0,0 +1,16 @@
+# Metodos uteis
+
+s1 = set()
+print(s1)
+
+# adiciona um item ao set
+s1.add("Luiz")
+print(s1)
+
+# Atualiza o set
+s1.update(("Lula", "maria"))
+print(s1)
+
+# descarta um item
+s1.discard("Lula")
+print(s1)
