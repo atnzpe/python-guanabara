37faedab8a98e518118fc3c2bbe70fb7117a283c
Gleyson Atanázio <gleysonasilva@gmail.com>
2023-11-22 16:20:29 -0300

    Cria enunciado

---
+++ calculo_valor_aluguel_carro.py
@@ -0,0 +1,6 @@
+'''
+Escreva um programa que pergunte a quantidade de KM percorrido por um
+carro alugado e a quantidade de dias pelos quais ele foi alugado.
+Calcule o Preço a pagar, sabendo que o carro custa R$ 60,00 por dia e R$ 0,15 por KM rodado.
+
+'''
