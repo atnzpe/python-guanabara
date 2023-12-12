380f1cd666827c1e000188c9e61192761bde106c
Gleyson Atanázio <gleysonasilva@gmail.com>
2023-12-12 17:32:49 -0300

    Atulza exemplo

--- exemplo_math.py
+++ exemplo_math.py
@@ -1,12 +1,15 @@
-#importa a biblioteca math
+# importa a biblioteca math
 import math
+import emoji
 
-#Solicita um numero inteiro e guarda va variavel num
+
+# Solicita um numero inteiro e guarda va variavel num
 num = int(input("Digite um número: "))
 
 #
 raiz_quadrada = math.sqrt(num)
 
-print(f'{raiz_quadrada =:.2f} do número {num}')
-print(f'{math.ceil(raiz_quadrada)} do número {num} arredondada para cima')
-print(f'{math.floor(raiz_quadrada)} do número {num} arredondada para baixo')
+print(emoji.emojize("Olá :sunglasses:", use_aliases=True))
+print(f"{raiz_quadrada =:.2f} do número {num} ")
+print(f"{math.ceil(raiz_quadrada)} do número {num} arredondada para cima")
+print(f"{math.floor(raiz_quadrada)} do número {num} arredondada para baixo")
