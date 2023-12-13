7db4a460b7ef9ea61a302683ff89cc46b50c26f4
Gleyson Atanázio <gleysonasilva@gmail.com>
2023-12-13 18:28:38 -0300

    atualiza o código

--- list_comprehension.py
+++ list_comprehension.py
@@ -2,3 +2,8 @@
 #Para lista adicione numero para nunero em nrange(10)
 lista = [numero for numero in range(10)]
 print(lista)
+
+
+#Para lista adicione numero e multiplica por 2 para nunero em range(10)
+lista = [numero * 2 for numero in range(10)]
+print(lista)
