57f83dc9b0a8bcdd30c0ce24ef5b25d5b1dee7c8
Gleyson Atanázio <gleysonasilva@gmail.com>
2024-01-17 16:09:27 -0300

    Cria enunciado para o desafio

---
+++ trabalhando_com_lista.py
@@ -0,0 +1,16 @@
+"""
+Cenário
+Os Beatles foram um dos grupos de música mais populares dos anos 60, e a banda mais vendida na história. Algumas pessoas consideram que eles são o ato mais influente da era do rock. Na verdade, eles foram incluídos na compilação da revista Time das 100 pessoas mais influentes do século XX.
+
+A banda passou por muitas mudanças na formação, culminando em 1962 com a formação de John Lennon, Paul McCartney, George Harrison e Richard Starkey (mais conhecido como Ringo Starr).
+
+
+Escreva um programa que reflita essas mudanças e permita praticar com o conceito de listas. Sua tarefa:
+
+#etapa 1: criar uma lista vazia chamada beatles;
+#etapa 2: use o método append() para adicionar os seguintes membros da banda à lista: John Lennon, Paul McCartney e George Harrison;
+#etapa 3: Use o loop for e o método append() para solicitar que o usuário adicione os seguintes membros da banda à lista: Stu Sutcliffe e Pete Best;
+#etapa 4: use a instrução del para remover Stu Sutcliffe e Pete Best da lista;
+#etapa 5: Use o método insert() para adicionar Ringo Starr ao início da lista.
+
+"""
