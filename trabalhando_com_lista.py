57e8129a767e7b163f2e3908481e163289c92cf2
Gleyson Atanázio <gleysonasilva@gmail.com>
2024-01-17 16:34:29 -0300

    Cria estapas 1 e 2

--- trabalhando_com_lista.py
+++ trabalhando_com_lista.py
@@ -14,3 +14,31 @@ Escreva um programa que reflita essas mudanças e permita praticar com o conceit
 #etapa 5: Use o método insert() para adicionar Ringo Starr ao início da lista.
 
 """
+
+#etapa 1: criar uma lista vazia chamada beatles;
+beatles = []
+print("Etapa 1:", beatles)
+
+#etapa 2: use o método append() para adicionar os seguintes membros da banda à lista: John Lennon, Paul McCartney e George Harrison;
+members = ['John Lennon','Paul McCartney','George Harrison']
+beatles = [members for members in members]
+print("Etapa 2:", beatles)
+
+
+#etapa 3: Use o loop for e o método append() para solicitar que o usuário adicione os seguintes membros da banda à lista: Stu Sutcliffe e Pete Best;
+    #Código com 
+print("Etapa 3:", beatles)
+
+
+# etapa 4
+
+print("Etapa 4:", beatles)
+
+# passo 5
+
+print("Etapa 5:", beatles)
+
+
+# testando o tamanho da lista
+
+("o fabuloso", len(beatles))
