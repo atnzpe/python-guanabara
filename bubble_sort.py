096141b13a1f0f78f0f76726a5d004aef5c479d0
Gleyson Atanázio <gleysonasilva@gmail.com>
2024-01-22 15:52:33 -0300

    Exemplo lista ordenada e reversa

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,5 +1,25 @@
-lista = [8,10,6,2,4]
-print(f'A lista esta desordenada {lista}')
+amor = [8, 10, 6, 2, 4]
+# print(f'A lista esta desordenada {lista}')
+# O metodo .sort ordena a lista
+amor.sort()
 
-for indice in range(len(lista) - 1):
- 
+print(amor)
+
+#amor.sort(reverse=True) deixa a lista invertida
+amor.sort(reverse=True)
+print(amor)
+# print(lista.sort(reverse=True))
+
+
+
+
+"""
+while troca:
+    troca = False
+    for i in range(len(lista) - 1):
+        if lista[i] > lista[i + 1]:
+            troca = True
+            lista[i], lista[i + 1] = lista[i + 1], lista[i]
+
+print(lista)
+"""
