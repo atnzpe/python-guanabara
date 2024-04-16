eeb551128fc3d2c2d11031bcaf93232b0381b7dc
Gleyson Atanázio <gleysonasilva@gmail.com>
2024-04-16 20:07:53 -0300

    define as classe suas heranças simples e multiplas

---
+++ exemplo_heranca_multipla_POO.py
@@ -0,0 +1,39 @@
+# Classe Pai
+class Animal:
+    pass
+
+# Classe filha de Animal
+class Ave(Animal):
+    pass
+
+# Classe Filha estende de animal
+class Mamifero(Animal):
+    pass
+
+
+# Classe filha de mamifero neta de animal
+class Cachorro(Mamifero):
+    pass
+
+
+# Classe filha de mamifero neta de animal
+class Gato(Mamifero):
+    pass
+
+
+# Classe filha de mamifero neta de animal
+class Leao(Mamifero):
+    pass
+
+
+# Classe filha de Animal
+class Ave(Animal):
+    pass
+
+
+
+
+
+#Classe que extende de mamifero e de ave
+class Onitorrinco(Mamifero, Ave):
+    pass
