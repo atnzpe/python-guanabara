7914b91b272fb724e21b85af24878a941d008a38
Gleyson Atanázio <gleysonasilva@gmail.com>
2024-04-16 21:06:24 -0300

    Ajustado código de herança simples e multipla na orientaçõa de objetos

--- exemplo_heranca_multipla_POO.py
+++ exemplo_heranca_multipla_POO.py
@@ -1,14 +1,29 @@
 # Classe Pai
 class Animal:
-    pass
+    # defino um construtor
+    def __init__(self, nr_patas):
+        self.nr_patas = nr_patas
+
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join([f'{chave} igual a {valor}' for chave, valor in self.__dict__.items()])}"
+
 
 # Classe filha de Animal
 class Ave(Animal):
-    pass
+    # defino um construtor
+    def __init__(self, cor_bico, **kw):
+        self.cor_bico = cor_bico
+        # chama construtor PAI
+        super().__init__(**kw)
+
 
 # Classe Filha estende de animal
 class Mamifero(Animal):
-    pass
+    # defino um construtor
+    def __init__(self, cor_do_pelo, **kw):
+        self.cor_do_pelo = cor_do_pelo
+        # chama construtor PAI
+        super().__init__(**kw)
 
 
 # Classe filha de mamifero neta de animal
@@ -27,13 +42,23 @@ class Leao(Mamifero):
 
 
 # Classe filha de Animal
-class Ave(Animal):
+
+
+# Classe que extende de mamifero e de ave
+class Onintorrinco(Mamifero, Ave):
     pass
 
 
+n_patas_gato = input("Qual o numero de patas do seu gato? ")
+cor_do_pelo_gato = input("Qual a cor do pêlo? ")
+n_patas_gato = int(n_patas_gato)
 
+n_patas_o = input("Qual o numero de patas do Onintorrinco? ")
+cor_do_pelo_o = input("Qual a cor do pêlo? ")
+cor_do_bico_o = input("Qual a cor do bico? ")
+n_patas_o = int(n_patas_o)
 
-
-#Classe que extende de mamifero e de ave
-class Onitorrinco(Mamifero, Ave):
-    pass
+jose_gato = Gato(nr_patas=n_patas_gato, cor_do_pelo=cor_do_pelo_gato)
+tony = Onintorrinco(nr_patas=n_patas_o, cor_do_pelo=cor_do_pelo_o, cor_bico=cor_do_bico_o)
+print(jose_gato)
+print(tony)
