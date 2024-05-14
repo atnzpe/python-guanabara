40fbc310ab5ce5889c70cd227c95aa2f5458b3cf
Gleyson Atanázio <gleysonasilva@gmail.com>
2024-05-14 19:59:32 -0300

    exemplo função interna

---
+++ exemplo_funcao_interna.py
@@ -0,0 +1,19 @@
+def executar(nome):
+    print(f'Executando a funcao Executar {nome}')
+    
+    def mensagem_curta():
+        print('Executa a Funçõa Mensagem')
+        
+
+    def mensagem_longa():
+        print ('Executa a mensagem longa')
+
+    mensagem_curta()
+    mensagem_longa()
+
+
+
+nome = input('Qual o seu nome? ')
+
+executar(nome)
+
