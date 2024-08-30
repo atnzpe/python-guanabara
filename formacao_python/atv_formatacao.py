3935512471b1b6d67a76a22ac89f42aaa7fbfe55
Gleyson Atanazio <gleysonasilva@gmail.com>
2024-08-30 11:16:59 -0300

    Exercicio formatação concluido com sucesso.

---
+++ formacao_python/atv_formatacao.py
@@ -0,0 +1,36 @@
+
+'''
+1 Escreva e formate a data em que você nasceu no formato dia/més/ano. 
+Não esqueça de criar 3 variáveis para guardar o dia, més e ano. 
+
+Exemplo de saída: Eu nasci em 30/9/1998 
+
+'''
+
+dia = 4
+mes = 11
+ano = 1983
+
+print("Eu nasci em {}/{}/{}".format(dia,mes,ano))
+
+'''
+2 Escreva e formate a hora e minuto atual. 
+Não esqueça de criar duas variáveis para guardar a hora e minuto. 
+
+Exemplo de saida: Agora são 22 horas e 37 minutos 
+'''
+hora =22
+minuto = 37
+
+print(f"Agora são {hora} horas e {minuto} minutos")
+
+'''
+3 Escreva um programa que contém o número PI, que deve ter o valor exato de 3.14159265359. 
+Agora formate esse número para ter apenas cinco casas decimais.. 
+
+Exemplo de saída: O PI 3.14159
+'''
+import math
+pi = math.pi
+
+print(f"O PI {pi:.5f}")
