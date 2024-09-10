db48813bd0deaef30b6c49d8ce7d3cae738f3ee5
Gleyson Atanázio <gleysonasilva@gmail.com>
2024-09-10 12:33:33 -0300

    atividade atualizada com sucesso

--- atividadew_operacao_aritimetica.py
+++ atividadew_operacao_aritimetica.py
@@ -44,16 +44,29 @@ Mostre esse valor na saida do programa.
 print(a)
 
 peso = float(input('Digite o peso: '))
-altura = float (input('Digite a altura em cm. (Ex: 178)): '))
-
+altura = float (input('Digite a altura em cm. (Ex: 1.78)): '))
+print(peso)
+print(altura)
 imc = peso/(altura * altura)
+print(imc)
 
-print (f'Seu IMC é: {imc:2.2f}')
+print (f'Seu IMC é: {imc:.2f}')
 
 
 
 
-'''
-4 (Desafio) Você tem um determinado números de ovos de páscoa para dividir entre um determinado número de pessoas (duas variáveis iniciais). Determine quantos ovos ficarão por pessoa e quantos ovos sobrarão pois não puderam ser divido igualmente. 
+a = '''
+>> Atividade 4
+Você tem um determinado números de ovos de páscoa para dividir entre um determinado número de pessoas (duas variáveis iniciais). 
+Determine quantos ovos ficarão por pessoa e quantos ovos sobrarão pois não puderam ser divido igualmente. 
 Lembre o número de ovos por pessoa é um número inteiro.
 '''
+
+print(a)
+
+ovos = int(input('Digite o numero de Ovos: '))
+pessoas = int(input('Digite o numero de Pessoas: '))
+ovos_por_pessoa = ovos // pessoas
+ovos_restantes = ovos % pessoas
+
+print(f'Cada pessoa deve receber {ovos_por_pessoa} Ovos de Páscoa e sobraram {ovos_restantes} ovos de páscoa')
