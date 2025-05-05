62ce5f1ef46b40ff3ffb8886ee4490b947dec5d6
Gleyson Atanázio <gleysonasilva@gmail.com>
2025-05-05 20:03:51 -0300

    formata documento

--- atividade_while.py
+++ atividade_while.py
@@ -1,6 +1,6 @@
-#Atividades
+# Atividades
 
-#1- Crie um programa que receba 5 números
+# 1- Crie um programa que receba 5 números
 # e retorne a média aritmética entre esses números
 
 soma_dos_numeros = 0
@@ -14,11 +14,11 @@ while numeros_lidos < 5:  # Cria um laço while
     numeros_lidos += 1
 
 media_aritimetica = soma_dos_numeros/5
-#executa a funçõa print da soma dos números com o valor exibindo duas cass decimais
+# executa a funçõa print da soma dos números com o valor exibindo duas cass decimais
 print(f"A soma dos números é {media_aritimetica:.2f}")
 
 
-#2 - Crie um programa que receba 5 números,
+# 2 - Crie um programa que receba 5 números,
 # realiza a soma destes números, mas caso um destes números seja negativo
 # não considere ele na soma.
 
@@ -28,22 +28,21 @@ contador = 0
 while contador < 5:
     numero_digitado = input("Digite um número: ")
     numero_digitado = float(numero_digitado)
-    
+
     if numero_digitado < 0:
         contador += 1
         continue
     else:
-        numeros_recebidos +=  numero_digitado
+        numeros_recebidos += numero_digitado
         contador += 1
-        
+
 print(f"A soma dos números é {numeros_recebidos:.2f}")
 
-#3 - Cria um programa que receba um número arbitrário (definido pelo usuário)
+# 3 - Cria um programa que receba um número arbitrário (definido pelo usuário)
 # de números que serão lidos e retorne a soma de todos eles.
 
 
+# 4 - Percorra os números de 2 até 10 e diga se o número é par ou impar.
 
-#4 - Percorra os números de 2 até 10 e diga se o número é par ou impar.
-
-#5- Crie um programa que receba como input uma string.
+# 5- Crie um programa que receba como input uma string.
 # Em seguida percorra a string e diga quantos espaços em branco essa string tem.
